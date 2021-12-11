05c5688e34afd6d4d38ebfb0f8f3797c6d56b054
Zhenkun Guo <1608229093@qq.com>
2021-12-11 10:28:51 +0800

    值加一

---
+++ numADD1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int Add(int* p)
+{
+	(*p)++;
+}
+int main()
+{
+	int num = 0;
+	Add(&num);
+	printf("num = %d\n", num);
+	Add(&num);
+	printf("num = %d\n", num);
+	Add(&num);
+	printf("num = %d\n", num);
+
+	return 0;
+}
